938bb83f498bd17edfb577d5801eb25017969f65
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-06-14 16:38:05 -0300

    v 1.48

---
+++ OS 150 EXERCÍCIOS DE PYTHON/Exercícios/ex001.py
@@ -0,0 +1,9 @@
+try:
+    num = int(input("Digite um número: "))
+    if num %2 ==0:
+        print("\033[36mEste número é par !\033[m")
+    else:
+        print("\033[33mEste número é ímpar!\033[m")
+    
+except:
+    print("\033[31mNão consegui calcular, verifique se você digitou apenas números !\033[m")
