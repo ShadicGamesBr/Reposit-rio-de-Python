4085a0e77d195e88fb02183486eb0515bb8ba3af
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-06-15 15:34:48 -0300

    v1.54

--- OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Ex014.py
+++ OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Ex014.py
@@ -1,16 +1,12 @@
 try:
     # 1, 2, 3, 5, 8, 13, 21, 34...
-    # 5, 8, 13, N, 34, 55, 89
-    sequencia = [1]
-    num = str(input("Quantos números você quer que eu gero ?")).strip()
-
+    sequencia = [1] 
+    num = 5
+    #num = str(input("Quantos números você quer que eu gere ?")).strip()
     while not num.isnumeric:
         num = str(input("Por favor, digite apenas números inteiros para funcionar :)")).strip()
-
     for i in range(1, int(num)+1):
         sequencia.append(sequencia[len(sequencia)-2]+ sequencia[len(sequencia)-1])
-
-
     print("Posição1: \033[34m1\033[m")
     for i,u in enumerate(sequencia):
         print(f"Posição{i+2}: \033[34m{sequencia[i]}\033[m")
---
+++ úteis/cores.py
@@ -0,0 +1,2 @@
+for i in range(0, 100, 1):
+    print(f"\033[{i}m■\033[m Número {i}")
