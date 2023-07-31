9d1c2e8c98aa23e53aa17b71788feeada1ea2a05
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-07-31 04:38:01 -0300

    v1.1

---
+++ OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Ex016.py
@@ -0,0 +1,8 @@
+# Pandas e Matplotlib
+from matplotlib import pyplot as plt
+import pandas as pd
+
+# Carregar os dados do arquivo
+notas = pd.read_excel(r"C:\Users\ShadicGamesBr\Documents\ESTUDOS\PYTHON\OS 150 EXERCÍCIOS DE PYTHON\Exercícios\Documentos\Tabela 1.xlsx")
+
+print(notas)
