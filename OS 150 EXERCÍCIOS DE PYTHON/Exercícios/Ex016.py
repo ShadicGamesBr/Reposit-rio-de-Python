527c6a2d281c74a5b2b0eca009cba36457410d0f
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-08-03 09:18:44 -0300

    v1.10

--- OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Ex016.py
+++ OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Ex016.py
@@ -1,17 +1,45 @@
 # Pandas e Matplotlib
 from matplotlib import pyplot as plt
 import pandas as pd
-import numpy as np
 
 # Carregar os dados do arquivo
-notas = pd.read_excel(r"C:\Users\ShadicGamesBr\Documents\ESTUDOS\PYTHON\OS 150 EXERCÍCIOS DE PYTHON\Exercícios\Documentos\Tabela 1.xlsx")
+lista = pd.read_excel(r"C:\Users\ShadicGamesBr\Documents\ESTUDOS\PYTHON\OS 150 EXERCÍCIOS DE PYTHON\Exercícios\Documentos\Tabela 1.xlsx")
 
-#Soma da coluna
+provisorio = {"Notas":([])}
 
-boletim = {"Matérias":[], "Notas":([])}
+#Pega cada célula da tabela pela quantidade de linhas e colunas
+boletim = {"Matérias":[], "Notas":[], "bimestre":[]}
+for coluna in range(0, lista.shape[0]):
+    for linha in range(0, len(lista.columns)):
+        provisorio["Notas"].append(lista.iloc[coluna, linha])
 
-for coluna in range(0, 8):
-    boletim["Matérias"].append(notas.iloc[coluna, 0])
+notas = []
 
-    
+#Separa notas e matérias
+for i in range(0, lista.shape[0]*5, 1):
+    if i%5 ==0:
+        boletim["Matérias"].append(provisorio["Notas"][i])
+    else:
+        notas.append(provisorio["Notas"][i])
+
+#Divide as notas de 4 em 4
+for bim in range(1, 5):
+    boletim["bimestre"].append(f"{bim}º bimestre")
+
+#Coloca as notas divididas no dicionário boletim
+for nota in range(0, (lista.shape[0]*4), 4):
+    boletim["Notas"].append(notas[nota:nota+4])
+
+
+
+#Verifica as características da tabela
+print(lista.shape[0]*5)
+print(len(notas))
+print(len(lista.columns))
 print(boletim)
+
+#Cria o gráfico do boletim
+plt.plot(boletim["Matérias"], boletim["Notas"])
+plt.title("Aproveitamento")
+plt.show()
+
--- consulta de RA/verRAs.py
+++ consulta de RA/verRAs.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from matplotlib import pyplot as plt
 from copy import deepcopy
+import pandas
 
 ra = '107058875'
 data = "23/06/2003"
@@ -12,33 +13,50 @@ driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 7)
 arte = []
 total = []
+anos = []
 for ano in range(2015, 2022):
     try:
-        try:
-            print(f"Ano de \033[34m{ano}\033[m")
-            driver.get(
-                f"https://sed.educacao.sp.gov.br/Boletim/GerarBoletimUnificadoExterno?nrRa={'000'+ra}&nrDigRa={digito}&dsUfRa=SP&dtNascimento={data}&nrAnoLetivo={000+ano}"),
-            print("\n")
-            notaBim = {}
-            notas_disciplina =[]
-            for notn in range(1, 13): #seleciona cada disciplina
-                for bim in range(2, 17, 4): #pega um número de 4 em 4 para selecionar a coluna certa para pegar as notas de cada bimestre
-                    bimnot = wait.until(EC.presence_of_all_elements_located((By.XPATH, f'/html/body/div/div/div[4]/table/tbody/tr[{notn}]/td[{bim}]')))[0].text
-                    if bimnot in ('10') or bimnot in ('123456789'):
-                        print(f"\033[32m{bimnot}\033[m", end=" ")
-                        notas_disciplina.append(bimnot)
-                    else:
-                        print(end="")
-                    if bimnot.isnumeric():
-                        total.append(int(bimnot))
-                    else:
-                        print("", end="")
-                print()
-        except:
-            print("\n\n\n")
+        print(f"Ano de \033[34m{ano}\033[m")
+        anos.append(f"{ano}")
+        driver.get(
+            f"https://sed.educacao.sp.gov.br/Boletim/GerarBoletimUnificadoExterno?nrRa={'000'+ra}&nrDigRa={digito}&dsUfRa=SP&dtNascimento={data}&nrAnoLetivo={000+ano}"),
+        print("\n")
+        notaBim = {}
+        notas_disciplina =[]
+        for notn in range(1, 13): #seleciona cada disciplina
+            for bim in range(2, 17, 4): #pega um número de 4 em 4 para selecionar a coluna certa para pegar as notas de cada bimestre
+                bimnot = wait.until(EC.presence_of_all_elements_located((By.XPATH, f'/html/body/div/div/div[4]/table/tbody/tr[{notn}]/td[{bim}]')))[0].text
+                if bimnot in ('10') or bimnot in ('123456789'):
+                    print(f"\033[32m{bimnot}\033[m", end=" ")
+                    notas_disciplina.append(bimnot)
+                else:
+                    print("-", end="")
+                if bimnot.isnumeric():
+                    total.append(int(bimnot))
+                else:
+                    total.append("-")
+                    print("", end="")
+            print()
     except:
-        print(f"\033[34m\033Não consegui ver este boletim![m")
+        print("\n\n\n")
 driver.quit()
-print(arte)
-print(f"\nMédia total: ")
-print(f"{(sum(total)/len(total)):.2f}")
+notasTotais =[]
+materias ={}
+
+for i in range(1, len(total)+1):
+    if i % 4 ==0:
+        notasTotais.append(total[i-4:i])
+for a in range(0, len(anos)):
+    if len(anos)-8 < a < len(anos)-3: #2015 a 2018
+        #print(f"Ano de : {anos[a]}\n{notasTotais[2*a:(2*a)+8]}\n")
+        materias[f"ano de {anos[a]}"] = notasTotais[a*8:a*8+8]
+    elif len(anos)-3 == a: #2019
+        materias[f"ano de {anos[a]}"] = notasTotais[32:44] #2019
+    elif len(anos)-2 ==a: #2020
+        materias[f"ano de {anos[a]}"] = notasTotais[44:55] #2020
+    elif len(anos)-1 ==a: #2021
+        materias[f"ano de {anos[a]}"] = notasTotais[55:67] #2021
+
+for chav, valor in materias.items():
+    print(f"Ano de : {chav}\n\t{valor}\n\n\n")
+ 
