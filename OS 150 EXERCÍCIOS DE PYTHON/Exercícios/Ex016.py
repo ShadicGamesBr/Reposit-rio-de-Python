9fd608c69a4ec6c99910a93eb6ac41f3b232a600
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-08-03 04:05:19 -0300

    v1.1

--- OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Ex016.py
+++ OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Ex016.py
@@ -1,8 +1,17 @@
 # Pandas e Matplotlib
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
 
 # Carregar os dados do arquivo
 notas = pd.read_excel(r"C:\Users\ShadicGamesBr\Documents\ESTUDOS\PYTHON\OS 150 EXERCÍCIOS DE PYTHON\Exercícios\Documentos\Tabela 1.xlsx")
 
-print(notas)
+#Soma da coluna
+
+boletim = {"Matérias":[], "Notas":([])}
+
+for coluna in range(0, 8):
+    boletim["Matérias"].append(notas.iloc[coluna, 0])
+
+    
+print(boletim)
