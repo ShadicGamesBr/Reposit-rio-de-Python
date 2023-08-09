684a4ae609375172ed3a53b09421f7126009ebce
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-08-09 11:56:49 -0300

    v2.1

--- OS 150 EXERCÍCIOS DE PYTHON/Exercícios/EX020.py
+++ OS 150 EXERCÍCIOS DE PYTHON/Exercícios/EX020.py
@@ -3,7 +3,9 @@
 from matplotlib import pyplot as plt
 import seaborn as sb
 from math import radians,cos,sin
-
+from matplotlib.animation import FuncAnimation
+from functools import partial
+import numpy as np
 #Vo = float(input("Velocidade do objeto (m/s): ")) #velocidade em M/S
 #Ang = float(input("Ângulo do objeto °: ")) #angulo
 Vo = 50
@@ -21,10 +23,25 @@ y = []
 
 for t in range(0, 21):
     x.append(t)
-    y.append(Voy*t - (0.5 * 9.8 *t**2)) #coloca cada valor de y no gráfico
+    y.append(Voy*t - (0.5 * 9.8 * t**2)) #coloca cada valor de y no gráfico
 
-plt.plot(x, y)
-plt.title("Movimento oblíquo")
-plt.xlabel("Tempo (s)")
-plt.ylabel("Altura ()")
+fig, ax = plt.subplots()
+line1, = ax.plot([], [], "ro")
+def init():
+    ax.set_xlim(0, 2*np.pi)
+    ax.set_ylim(-1, 1)
+    return line1,
+
+def update(frame, ln, x, y):
+    x.append(frame)
+    y.append(np.sin(frame))
+    ln.set_data(x, y)
+    return ln,
+
+ani = FuncAnimation(
+    fig, partial(update, ln=line1, x=[], y=[]),
+    frames = np.linspace(0, 2*np.pi, 128),
+    init_func=init, blit=True
+)
+
 plt.show()
