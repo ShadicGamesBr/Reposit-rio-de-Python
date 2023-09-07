1f21a828a4dda13793268e017c238a0c8df0f563
ShadicGamesBr <fk054590@gmail.com>
2023-09-07 02:09:16 -0300

    aprendi algo

--- OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Python Básico/ex001.py
+++ OS 150 EXERCÍCIOS DE PYTHON/Exercícios/Python Básico/ex001.py
@@ -1,9 +1,10 @@
 try:
     num = int(input("Digite um número: "))
-    if num %2 ==0:
+    if num % 2 == 0:
         print("\033[36mEste número é par !\033[m")
     else:
         print("\033[33mEste número é ímpar!\033[m")
-    
-except:
-    print("\033[31mNão consegui calcular, verifique se você digitou apenas números !\033[m")
+
+except ImportError:
+    print(
+        "\033[31mNão consegui calcular, verifique se você digitou apenas números !\033[m")
